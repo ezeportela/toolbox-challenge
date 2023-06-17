bd917754b598d7cfe9cbfe0259a3418a4e0cccae
Ezequiel Portela <ezequiel.portela@uala.com.ar>
2023-06-17 17:30:13 -0300

    update dockerfiles & order file list

--- api/domain/file.repository.js
+++ api/domain/file.repository.js
@@ -37,7 +37,18 @@ class FileRepository {
       .slice(1)
       .map(this.processFile, this)
       .filter((file) => !_.isEmpty(file))
-    return _.orderBy(unorderedFiles, _.first(this.columns))
+    return unorderedFiles.slice().sort(this.sortFileList.bind(this))
+  }
+
+  processFile (fileMetadata) {
+    const rows = fileMetadata.split('\n').map((row) => row.split(','))
+
+    const lines = rows
+      .filter((row) => this.validateFileRow(row), this)
+      .map((row, idx) => this.parseFileRow(idx, row), this)
+      .reduce((prev, curr) => _.merge(prev, curr), {})
+
+    return lines
   }
 
   validateFileRow (row) {
@@ -60,15 +71,23 @@ class FileRepository {
       .reduce((prev, curr) => _.merge(prev, curr), {})
   }
 
-  processFile (fileMetadata) {
-    const rows = fileMetadata.split('\n').map((row) => row.split(','))
+  sortFileList (a, b) {
+    const col = _.first(this.columns)
+    const fileA = _.get(a, col)
+    const fileB = _.get(b, col)
 
-    const lines = rows
-      .filter((row) => this.validateFileRow(row), this)
-      .map((row, idx) => this.parseFileRow(idx, row), this)
-      .reduce((prev, curr) => _.merge(prev, curr), {})
+    const numA = Number(fileA.match(/\d+/))
+    const numB = Number(fileB.match(/\d+/))
 
-    return lines
+    const strA = fileA.replace(numA, '')
+    const strB = fileB.replace(numB, '')
+    const strComparison = strA.localeCompare(strB)
+
+    if (strComparison === 0) {
+      return numA - numB
+    }
+
+    return strComparison
   }
 }
 
